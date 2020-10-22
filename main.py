8c01b424521905a107dfa54683ff13db875e0877
Jiubei0408 <757795210@qq.com>
2020-10-22 10:25:18 +0800

    implemented

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import re
+import json
 import base64
 from spider_http import SpiderHttp
 
 spider_http = SpiderHttp()
 
 
-def login(username, password, contest_id):
+def get_data(full_file_name, username, password, contest_id):
     code = base64.b64encode('{}:{}'.format(username, password).encode('utf-8')).decode('utf-8')
     spider_http.headers.update({
         'Authorization': 'Basic {}'.format(code)
     })
-    url = 'http://10.66.28.6:12345/api/v4/contests/{}/scoreboard?public=false'.format(contest_id)
-    res = spider_http.get(url=url)
-    print(res.json())
-    url = 'http://10.66.28.6:12345/api/v4/contests/{}/scoreboard?public=true'.format(contest_id)
-    res = spider_http.get(url=url)
-    print(res.json())
+    result = {
+        'success': False,
+        'data': {}
+    }
+    try:
+        url = 'http://10.66.28.6:12345/api/v4/contests/{}/scoreboard?public=false'.format(contest_id)
+        res = spider_http.get(url=url)
+        result['data']['final'] = res.json()['rows']
+        url = 'http://10.66.28.6:12345/api/v4/contests/{}/scoreboard?public=true'.format(contest_id)
+        res = spider_http.get(url=url)
+        result['data']['pending'] = res.json()['rows']
+        result['success'] = True
+    except Exception as e:
+        result['data']['error'] = str(e)
+    with open(full_file_name, 'w') as fp:
+        json.dump(result, fp)
 
 
 if __name__ == '__main__':
-    login('smz', 'smzsmzsmz', 5)
+    from secure import username, password
+    get_data('data.json', username, password, 5)
---
+++ secure_demo.py
@@ -0,0 +1,4 @@
+# 将本文件复制后重命名为secure.py并填写管理员账户的用户名密码
+
+username = ''
+password = ''
